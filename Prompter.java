67bbdb225b31da8f1f0ad53a050105511d505e27
Muhammad Ammar Aamir <ammar2411@gmail.com>
2016-07-19 17:20:09 +0500

    Player won't be able to guess the same letter more than once.

--- Prompter.java
+++ Prompter.java
@@ -16,9 +16,20 @@ public class Prompter {
 
   public boolean promptForGuess() {
     Console console = System.console();
-    String guessAsString = console.readLine("Enter a letter:  ");
-    char guess = guessAsString.charAt(0);
-    return mGame.applyGuess(guess);
+    boolean isHit = false;
+    boolean isValidGuess = false;
+    while(! isValidGuess) {
+      String guessAsString = console.readLine("Enter a letter:  ");
+      char guess = guessAsString.charAt(0);
+      try {
+        isHit = mGame.applyGuess(guess);
+        isValidGuess = true;
+      } catch (IllegalArgumentException iae) {
+        console.printf("%s. Please try again. \n", iae.getMessage());
+      }
+    }
+
+    return isHit;
   }
 
   public void displayProgress() {
